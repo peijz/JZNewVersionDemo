a1754d4250d37d0262cb7d3986bdd338db4ed77b
wanhongios <120761605@qq.com>
2017-10-26 14:23:18 +0800

    demo代码

--- JZNewVersionDemo/ViewController.m
+++ JZNewVersionDemo/ViewController.m
@@ -20,6 +20,11 @@
     self.view.backgroundColor = [UIColor brownColor];
     
     JZNewVersionView *jz_newVersionView = [[JZNewVersionView alloc]initWithFrame:self.view.bounds datas:[NSArray arrayWithObjects:@"u=2771784367,1529189070&fm=27&gp=0.jpg",@"u=2398155085,3520102918&fm=27&gp=0.jpg",@"u=2514746018,3381081595&fm=27&gp=0.jpg",@"u=4266564208,2452097814&fm=11&gp=0.jpg",@"u=87941439,1125787948&fm=11&gp=0.jpg", nil]];
+    jz_newVersionView.jz_enterBtnTitle = @"点击这里";
+    jz_newVersionView.jz_enterBtnBackColor = [UIColor redColor];
+    jz_newVersionView.jz_enterBtnTitleFont = 22;
+    jz_newVersionView.jz_enterBtnTitleColor = [UIColor blackColor];
+    jz_newVersionView.jz_enterBtnBorderColor = [UIColor greenColor];
     [jz_newVersionView newVersionViewEnterBlock:^{
         NSLog(@"点击了版本新特性");
     }];
